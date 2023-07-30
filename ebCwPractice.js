6fa0411efa2504ecf9f179161729c51b5493e021
Scott Nelson <scottninlv@yahoo.com>
2023-07-30 13:15:13 -0700

    Another question in the books

--- ebCwPractice.js
+++ ebCwPractice.js
@@ -6,9 +6,32 @@
 
 // console.log(sumPolygon(6))
 
-
 // Problem Two
 
-const squared = (n) => Math.pow(n, 2)
+// const squared = (n) => Math.pow(n, 2)
 
-console.log(squared(6))
+// console.log(squared(6))
+
+
+
+// function squared(n) {
+//   return n * n 
+// }
+
+// console.log(squared(2))
+
+
+
+
+// Problem Three
+
+
+function hoopCount(n) {
+    if( n < 10) {
+        return ("Keep at it until you get it")
+    } else {
+        return ("Great, now move on to tricks")
+    }
+}
+
+console.log(hoopCount(12))
